e27e345563f2504f1b0061697f03caa9b00265f8
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-01 12:13:31 -0300

    add context, and ideas for its use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import DataContext from './context/DataContext'
 
 function App() {
-  return <></>
+  const [data, setData] = useState(null)
+  // useEffect(() => {
+  // gets stuff from local storage and checks if there's data
+  // if no data do nothing
+  // if data setData(data)
+  //})
+  return (
+    <>
+      <DataContext.Provider value={{ data, setData }}>
+        <div>Hello World</div>
+      </DataContext.Provider>
+    </>
+  )
 }
 
 export default App
