198df99777a8c6b81e5c55ee048d1528aff02594
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-02 11:29:31 -0300

    start work on Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from 'react'
+import { Header, Footer, CvItem } from '../components'
+import styled from 'styled-components'
+import DataContext from '../context/DataContext'
+
+const EmptyMessage = styled.div``
+
+const HomeBg = styled.div`
+  background-color: #fff000;
+`
+const HomeContainer = styled.div``
+
+const NewCVButton = styled.button``
+
+export default function Home(props) {
+  const { data } = useContext(DataContext)
+
+  return (
+    <HomeBg>
+      <Header {...props} />
+      <HomeContainer>
+        {data === null ? (
+          <EmptyMessage>
+            It seems you have no CVs. Create you first one!
+          </EmptyMessage>
+        ) : (
+          data.map((item, i) => (
+            <CvItem
+              key={`${item} - ${i}`}
+              data-id={i}
+              itemData={item}
+              onClick={() => {
+                props.setInHome(false)
+                props.setInPreview(true)
+              }}
+            />
+          ))
+        )}
+        <NewCVButton
+          onClick={() => {
+            props.setInHome(false)
+            props.setInCreating(true)
+          }}
+        >
+          NEW CV
+        </NewCVButton>
+      </HomeContainer>
+      <Footer />
+    </HomeBg>
+  )
+}
