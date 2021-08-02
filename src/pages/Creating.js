ed1c9fc1bcdb5f0f365ac96caa0c70cbf67a55f6
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-02 12:20:10 -0300

    add most top level logic to Creating

--- src/pages/Creating.js
+++ src/pages/Creating.js
@@ -1,6 +1,69 @@
-import React, { useState } from 'react'
-import { Header, Footer } from '../components'
+import React, { useState, useContext } from 'react'
+import styled from 'styled-components'
+import DataContext from '../context/DataContext'
+import {
+  Education,
+  Experience,
+  Footer,
+  GeneralInfo,
+  Header,
+  ProgressBar,
+  Skills,
+} from '../components'
 
 export default function Creating(props) {
-  return <div>i'm Creating</div>
+  const { setCurrentCv } = useContext(DataContext)
+  const [inGeneralInfo, setInGeneralInfo] = useState(true)
+  const [inEducation, setInEducation] = useState(false)
+  const [inExperience, setInExperience] = useState(false)
+  const [inSkills, setInSkills] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+  const CreatingBg = styled.div``
+
+  const CreatingContainer = styled.div``
+
+  return (
+    <CreatingBg>
+      <Header {...props} />
+      <CreatingContainer>
+        <ProgressBar progress={progress} />
+        {inGeneralInfo && (
+          <GeneralInfo
+            setInGeneralInfo={setInGeneralInfo}
+            setInEducation={setInEducation}
+            setCurrentCv={setCurrentCv}
+            setProgress={setProgress}
+          />
+        )}
+        {inEducation && (
+          <Education
+            setInEducation={setInEducation}
+            setInExperience={setInExperience}
+            setCurrentCv={setCurrentCv}
+            setProgress={setProgress}
+          />
+        )}
+        {inExperience && (
+          <Experience
+            setInExperience={setInExperience}
+            setInSkills={setInSkills}
+            setCurrentCv={setCurrentCv}
+            setProgress={setProgress}
+          />
+        )}
+        {inSkills && (
+          <Skills
+            setInSkills={setInSkills}
+            setInExperience={setInExperience}
+            setCurrentCv={setCurrentCv}
+            setInCreating={props.setInCreating}
+            setInPreview={props.setInPreview}
+            setProgress={setProgress}
+          />
+        )}
+      </CreatingContainer>
+      <Footer />
+    </CreatingBg>
+  )
 }
