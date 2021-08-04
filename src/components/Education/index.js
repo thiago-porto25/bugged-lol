ff2a0ccd38d77aeb0e3835b3f623725f135dd48c
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-04 12:24:28 -0300

    create logic for general info page

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -1,5 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  AddButton,
+  Box,
+  Container,
+  Form,
+  Input,
+  Label,
+  NextButton,
+  RemoveButton,
+  Text,
+  Title,
+} from './styles/Education'
 
 export default function Education(props) {
+  useEffect(() => {
+    props.setProgress(2)
+  }, [])
+
   return <div>i'm Education</div>
 }
+
+/*
+ter degree, nome da instituicao, ano de entrada, ano de conclusao. se cursando deixar em branco
+*/
